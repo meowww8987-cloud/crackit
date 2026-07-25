d497da2316fc369a4d64a4ffed24b2c9f0648f96
Z User <z@container>
2026-07-25 09:18:46 +0000

    336406bd-2998-4d1e-8fb2-43e10d343126

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,26 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['*'],
   // Disable HMR to prevent stale module cache issues
   devIndicators: false,
+  // Exclude heavy optional deps from the standalone server bundle.
+  // - sharp: only needed for next/image optimization (we don't use it)
+  // - @img: same as above
+  // - Prisma non-SQLite engines: we only use SQLite
+  // This shaves ~95MB off the deployment.
+  outputFileTracingExcludes: {
+    '*': [
+      './node_modules/typescript/**/*',
+      './node_modules/@img/**/*',
+      './node_modules/sharp/**/*',
+      './node_modules/@prisma/client/runtime/query_engine_bg.mysql.wasm*',
+      './node_modules/@prisma/client/runtime/query_engine_bg.postgresql.wasm*',
+      './node_modules/@prisma/client/runtime/query_engine_bg.cockroachdb.wasm*',
+      './node_modules/@prisma/client/runtime/query_engine_bg.sqlserver.wasm*',
+      './node_modules/@prisma/client/runtime/query_compiler_bg.mysql.wasm*',
+      './node_modules/@prisma/client/runtime/query_compiler_bg.postgresql.wasm*',
+      './node_modules/@prisma/client/runtime/query_compiler_bg.cockroachdb.wasm*',
+      './node_modules/@prisma/client/runtime/query_compiler_bg.sqlserver.wasm*',
+    ],
+  },
 };
 
 export default nextConfig;
